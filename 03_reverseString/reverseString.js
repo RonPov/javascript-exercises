a7a7d1f9c6bc1011c640d9868bdca270606682d1
RONPOV <172224934+RonPov@users.noreply.github.com>
2025-01-03 20:49:16 -0500

    Resolve exercises 1,2 and 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let result = '';
+    let reverted = string.split('');
+    reverted = reverted.reverse();
+    for(i of reverted){
+        result += i
+    }
+    return result;
 };
 
 // Do not edit below this line
