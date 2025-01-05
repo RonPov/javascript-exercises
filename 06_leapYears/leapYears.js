f68e80c9073558473f39677cb7da9785c23b9897
RONPOV <172224934+RonPov@users.noreply.github.com>
2025-01-04 23:29:27 -0500

    resolve exercise number 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if(year/4 == Math.floor(year/4)){
+        if(year/100 == Math.floor(year/100)){
+            if(year/400 == Math.floor(year/400)){
+                return true
+            } else{
+                return false
+            }
+        }else{
+            return true
+        }
+    } else{
+        return false
+    }
 };
 
 // Do not edit below this line
