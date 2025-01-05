bb47dd22e109a63ea20de62a50ac09de9c9b7376
RONPOV <172224934+RonPov@users.noreply.github.com>
2025-01-04 22:17:29 -0500

    resolve exercise number 3

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(list, other) {
+    for(i=1; i<arguments.length; i++){
+        while(list.includes(arguments[i])){
+            let index = list.indexOf(arguments[i]);
+            list.splice(index, 1)
+        }
+   
+
+};
+
+return list
+
 };
 
 // Do not edit below this line
